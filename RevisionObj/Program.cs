8fc65cff0696cd3411e2f02d176b3d5f8c0898ba
Carliertheo <carlier85640@gmail.com>
2021-03-25 11:21:10 +0100

    nouvelmodif

--- RevisionObj/Program.cs
+++ RevisionObj/Program.cs
@@ -2,11 +2,47 @@
 
 namespace RevisionObj
 {
+    
+    class A 
+    {
+        public string s = "hello world";
+        public static int reponseatouteslesquestion = 42; 
+
+        public override string ToString()
+        {
+            return "Coucou";
+        }
+
+        public void test(string ss)
+        {
+            Console.WriteLine(ToString() + " " + ss);
+        }
+
+        public static void testclass()
+        {
+            new A().test("Bonjour tout le monde");
+        }
+    }
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // if (args.Length > 0)
+            // {
+            //     Console.WriteLine("hello "+ args[0] + " !");
+            // }
+            // Console.WriteLine("Hello World!");
+
+            A a;
+            a = new A();
+
+            Console.WriteLine(new A().ToString());
+            Console.WriteLine(a.s);
+            a.test("Theo");
+            A.testclass();
         }
     }
 }
